07adcefe9af99e3eef5a46c191a66ffb86f4fe57
sobotredsystems <sobotred.systems@gmail.com>
2022-09-05 23:32:13 -0400

    actualizacion2

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
   <!-- /.login-logo -->
   <div class="card card-outline card-primary">
     <div class="card-header text-center">
-      <a href="plantilla/index2.html" class="h1"><b>Admin</b>LTE</a>
+      <a href="plantilla/index2.html" class="h1"><b>MVC </b>LTE</a>
     </div>
     <div class="card-body">
       <p class="login-box-msg">Sign in to start your session</p>
